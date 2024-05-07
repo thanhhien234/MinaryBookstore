28131c666a7bbc6894a05efc4d9e8ad3af7589f6
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-08 00:31:14 +0900

    feat: header toggle placeholder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 function Header() {
+  const [searchType, setSearchType] = useState('isbn');
+  const handleSearchSelect = (event) => {
+    setSearchType(event.target.value);
+  }
   return (
     <div className="header-container">
         <div className="search-container">
             <img src={require("../assets/images/logo.png")} alt=""/>
             <div className="search-wrapper">
-                <select id="search-type">
+                <select id="search-type" onChange={handleSearchSelect}>
                     <option value="isbn"><span>ISBN 검색</span></option>
                     <option value="title">제목 검색</option>
                 </select>
-                <input type="text" className="search-input" placeholder="ISBN 13자리 숫자를 입력하세요."/>
+                <input type="text" className="search-input" placeholder={searchType === 'isbn' ? 'ISBN 13자리 숫자를 입력하세요.' : '책 제목을 입력하세요.'} />
                 <button className="search-btn">검색</button>
             </div>
             
