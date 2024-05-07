9d761c7ea7cf02cc5080374e09e9fa77dda41f6f
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-08 00:10:25 +0900

    feat: delete AddBook page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './routes/Home';
 import Header from './components/Header';
-import AddBook from './routes/AddBook';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/analysis' element={<AddBook/>}/>
       </Routes>    
     </div>
   );
--- src/routes/AddBook.js
+++
@@ -1,8 +0,0 @@
-function AddBook() {
-    return (
-      <div>
-        <h1>AddBook</h1>
-      </div>
-    );
-  }
-  export default AddBook;
