98bbc9b6224334ffb5c0581ae75000ac942f9623
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-15 21:02:02 +0900

    feat: isbn search api

--- src/routes/BookSale.js
+++ src/routes/BookSale.js
@@ -2,13 +2,64 @@ import React, {useState,useEffect} from "react";
 import './BookSale.css';
 import ConditionRadioList from '../components/ConditionRadioList';
 import UploadImage from "../components/UploadImage";
+import { getCookie } from "../utils/cookieManage";
 
 function BookSale() {
   const [activeTab, setActiveTab] = useState('isbn');
+  const [isbnBookInfo, setIsbnBookInfo] = useState(null);
+  const [isbn, setIsbn] = useState(''); 
+  const [publicationDate, setPublicationDate] = useState('');
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  
+  const isbnSearch = () => {
+    const isbnValue = document.querySelector('.isbn-input').value;
+    if (isbnValue === '') {
+      alert('ISBN 코드를 입력해주세요.');
+      return;
+    }
+    else {
+      setIsbn(document.querySelector('.isbn-input').value);
+    }
+    fetch(`${process.env.REACT_APP_API_URL}/api/isbn?isbn=${isbnValue}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + getCookie('accessToken'),
+      }
+    })
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      else if (response.status === 404) {
+        alert('해당 도서가 없습니다. 다시 입력하세요.');
+      }
+      else{
+        throw new Error();
+      }
+    })
+    .then(data => {
+      setIsbnBookInfo(data);
+    })
+    .catch(error => {
+      alert('서버 오류입니다. 잠시 후 다시 시도해주세요.');
+    });
+  }
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString('ko-KR', options);
+  }
+  const handlePublicationDateChange = (event) => {
+    let value = event.target.value;
+    value = value.replace(/\D/g, "")
+    value = value.replace(/^(\d{4})(\d{1,2})/, "$1-$2");
+    value = value.replace(/^(\d{4}-\d{2})(\d{1,2})/, "$1-$2");
+    setPublicationDate(value);
+  };
 
   return (
     <div className="book-sale-main-container">
@@ -20,52 +71,54 @@ function BookSale() {
         {activeTab === 'isbn' ? (
           <div className="isbn-wrapper">
             <div className="isbn-input-wrapper">
-              <input type="text" className="isbn-input" placeholder="ISBN 코드를 입력하세요" />
-              <img src={require('../assets/icons/search.png')} alt="" className="isbn-search-icon" />
+              <input type="number" className="isbn-input" placeholder="ISBN 코드를 입력하세요" />
+              <img src={require('../assets/icons/search.png')} alt="" className="isbn-search-icon" onClick={isbnSearch}/>
             </div>
-            <div className="book-info-wrapper">
-              <div className="book-info-inner">
-                  <h2 className="book-title">한국어능력시험 HOT TOPIK 2: 읽기</h2>
-                  <p><span className="label">ISBN</span><span id="isbn" className="info-value">9791186701355</span></p>
-                  <p><span className="label">저자</span><span id="author" className="info-value">김순례,조민지</span></p>
-                  <p><span className="label">출판사</span><span id="publisher" className="info-value">한글파크</span></p>
-                  <p><span className="label">출판일</span><span id="publicationDate" className="info-value">2022년 1일 1월</span></p>
-                  <p><span className="label">정가</span><span id="price" className="info-value">16800원</span></p>
+            {isbn && isbnBookInfo && (
+              <div className="book-info-wrapper">
+                  <div className="book-info-inner">
+                      <h2 className="book-title">{isbnBookInfo.title}</h2>
+                      <p><span className="label">ISBN</span><span id="isbn" className="info-value">{isbn}</span></p>
+                      <p><span className="label">저자</span><span id="author" className="info-value">{isbnBookInfo.author.join(', ')}</span></p>
+                      <p><span className="label">출판사</span><span id="publisher" className="info-value">{isbnBookInfo.publisher}</span></p>
+                      <p><span className="label">출판일</span><span id="publicationDate" className="info-value">{formatDate(isbnBookInfo.publicationDate)}</span></p>
+                      <p><span className="label">정가</span><span id="price" className="info-value">{isbnBookInfo.price}원</span></p>
+                  </div>
+                <div className="book-cover">
+                    <img src={isbnBookInfo.img} alt="" />
+                </div>
               </div>
-              <div className="book-cover">
-                  <img src={require('../assets/images/exampleBook.png')} alt="" />
-              </div>
-            </div>
+            )}
           </div>
         ) : (
           <div className="title-wrapper">
               <div className="form-group">
                 <label htmlFor="titleInput">제목</label>
-                <input type="text" id="titleInput"/>
+                <input type="text" id="titleInput" placeholder="예: 명품 웹 프로그래밍"/>
               </div>
               <div className="form-group">
                 <label htmlFor="isbn">ISBN</label>
-                <input type="text" id="isbn"/>
+                <input type="number" id="isbn" placeholder="예: 9788970505459"/>
               </div>
               <div className="form-group">
                 <label htmlFor="author">저자</label>
-                <input type="text" id="author"/>
+                <input type="text" id="author" placeholder="예: 황기태"/>
               </div>
               <div className="form-group">
                 <label htmlFor="publisher">출판사</label>
-                <input type="text" id="publisher"/>
+                <input type="text" id="publisher" placeholder="예: 생능출판"/>
               </div>
               <div className="form-group">
                 <label htmlFor="publicationDate">출판일</label>
-                <input type="text" id="publicationDate"/>
+                <input type="text" id="publicationDate" placeholder="예: 2022-02-10" value={publicationDate} onChange={handlePublicationDateChange} maxLength="10"/>
               </div>
               <div className="form-group">
                 <label htmlFor="price">정가</label>
-                <input type="number" id="price"/>원
+                <input type="number" id="price" placeholder="예: 30000"/>원
               </div>
               <div className="form-group">
                 <label htmlFor="salePrice">판매가</label>
-                <input type="number" id="salePrice"/>원
+                <input type="number" id="salePrice" placeholder="예: 20000"/>원
               </div>
           </div>
         )}
@@ -76,7 +129,7 @@ function BookSale() {
         {activeTab === 'isbn' && (
           <div className="book-price-wrapper">
             <h3>판매 가격을 입력해주세요</h3>
-            <input type="number" className="price-input" placeholder="16800" />
+            <input type="number" className="price-input" placeholder={isbnBookInfo ? isbnBookInfo.price : ""} />
             <span>원</span>
           </div>
         )}
