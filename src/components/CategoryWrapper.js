f19116d887372e82dcf203bbac58eb7f5a77146f
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-21 02:36:34 +0900

    feat: render data in components

--- src/components/CategoryWrapper.js
+++ src/components/CategoryWrapper.js
@@ -1,58 +1,24 @@
-import React from "react";
-import './CategoryWrapper.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CategoryWrapper.css';
+import { categoryList } from '../routes/Home';
 
-function CategoryWrapper() {
+function CategoryWrapper({ activeStatus }) {
+
     return (
         <div className="category-wrapper">
-            <div className='category-item'>
-                <img src={require('../assets/icons/novel.png')} alt=""/>
-                <span>소설</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets//icons/literature.png')} alt=""/>
-                <span>인문</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/computer.png')} alt=""/>
-                <span>컴퓨터/IT</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/language.png')} alt=""/>
-                <span>외국어</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/history.png')} alt=""/>
-                <span>역사/문화</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/science.png')} alt=""/>
-                <span>과학</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/magazine.png')} alt=""/>
-                <span>잡지</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/children.png')} alt=""/>
-                <span>어린이</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/headphone.png')} alt=""/>
-                <span>자기개발</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/travel.jpg')} alt=""/>
-                <span>여행</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/cooking.png')} alt=""/>
-                <span>요리</span>
-            </div>
-            <div className='category-item'>
-                <img src={require('../assets/icons/star.png')} alt=""/>
-                <span>기타</span>
-            </div>
+            {categoryList.map((category, index) => (
+                <Link
+                    key={index}
+                    className='category-item'
+                    to={`/book-list/${activeStatus}/${category.name}`}
+                >
+                    <img src={category.icon} alt={category.label} />
+                    <span>{category.label}</span>
+                </Link>
+            ))}
         </div>
     );
 }
+
 export default CategoryWrapper;
