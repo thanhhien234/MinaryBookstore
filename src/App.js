f6cf002a8215d46370a69ddca51bd65570174ea8
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-27 17:37:32 +0900

    fix: remove AuthContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import { useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
-import { AuthProvider } from './contexts/AuthContext';
+import { useDispatch } from 'react-redux';
+import { setUser } from './store/slices/userSlice';
+import { getCookie, setCookie } from './utils/cookieManage';
+import { getUserInfoApi } from './api/getUserInfoApi';
 import Header from './components/Header';
 import Home from './routes/Home';
 import DetailBook from './routes/DetailBook';
@@ -12,22 +16,50 @@ import SearchBookList from './routes/SearchBookList';
 import Redirect from './routes/Redirect';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!getCookie("accessToken") && getCookie("refreshToken")) {
+      fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getCookie("refreshToken")}`
+        }
+      })
+        .then(response => response.json())
+        .then(res => {
+          setCookie("accessToken", res.accessToken, 2 * 60);
+          setCookie("refreshToken", res.refreshToken, 24 * 14 * 60);
+          getUserInfoApi()
+            .then(res => dispatch(setUser({ name: res.name, img: res.img })))
+            .catch(error => console.log(error));
+        })
+        .catch(error => {
+          alert("관리자에게 문의해주세요.");
+        });
+    }
+    else {
+      getUserInfoApi()
+        .then(res => dispatch(setUser({ name: res.name, img: res.img })))
+        .catch(error => console.log(error));
+    }
+  }, []);
+
   return (
-    <AuthProvider>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/detail-book/:bookState/:bookId' element={<DetailBook />} />
-          <Route path='/create-book/:option' element={<CreateBook />} />
-          <Route path='/book-list/:status/:category' element={<BookList />} />
-          <Route path='/search-book-list/:option/:input' element={<SearchBookList />} />
-          <Route path='/my-page' element={<MyPage />} />
-          <Route path='/edit-book/:option/:bookId' element={<EditBook />} />
-          <Route path='/redirect' element={<Redirect />} />
-        </Routes>
-      </div>
-    </AuthProvider>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/detail-book/:bookState/:bookId' element={<DetailBook />} />
+        <Route path='/create-book/:option' element={<CreateBook />} />
+        <Route path='/book-list/:status/:category' element={<BookList />} />
+        <Route path='/search-book-list/:option/:input' element={<SearchBookList />} />
+        <Route path='/my-page' element={<MyPage />} />
+        <Route path='/edit-book/:option/:bookId' element={<EditBook />} />
+        <Route path='/redirect' element={<Redirect />} />
+      </Routes>
+    </div>
   );
 }
 export default App;
